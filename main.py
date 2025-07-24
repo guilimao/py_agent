005933e8d0356ad999c0bc227b4d32ec473f75eb
Guilimao_Official <1137056144@qq.com>
2025-07-24 21:33:02 +0800

    添加了kimi k2模型

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument(
         "--model",
         type=str,
-        default="kimi-k2-0711-preview",
+        default="kimi-k2-250711",
         help="指定使用的LLM模型名称"
     )
     args = parser.parse_args()
